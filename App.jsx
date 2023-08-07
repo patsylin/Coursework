83073cd9a968007ae1adac07133ee805a11c020f
Patsy Lin <97324605+patsylin@users.noreply.github.com>
2023-08-06 21:19:27 -0500

    vite-project

---
+++ App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+import {puppyList} from './data.js'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+
+  return (
+    <div className="App">
+{puppies.map((puppy) => {
+  return (
+    <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>
+      {puppy.name}
+    </p>
+  );
+})}
+
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+   </div>
+  );
+}
+
+export default App
